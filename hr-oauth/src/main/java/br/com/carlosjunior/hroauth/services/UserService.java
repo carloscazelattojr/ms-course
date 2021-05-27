93839e4dbfe700be43e5b49f796787c2cbe41825
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-05-26 23:12:08 -0300

    Login, JWT Token configured

--- hr-oauth/src/main/java/br/com/carlosjunior/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/br/com/carlosjunior/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package br.com.carlosjunior.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.com.carlosjunior.hroauth.entities.User;
 import br.com.carlosjunior.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -26,5 +29,17 @@ public class UserService {
 		logger.info("E-mail found: " + email);
 		return user;
 
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+
+		if (user == null) {
+			logger.error("E-mail not found: " + username);
+			throw new UsernameNotFoundException("E-mail not found!!!");
+		}
+		logger.info("E-mail found: " + username);
+		return user;
 	}
 }
